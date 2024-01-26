fccda74999584d7d3f6883ce4cf96c965c82fa7d
santiagoweb212 <juanusca45@gmail.com>
2024-01-25 19:31:10 -0500

    refactor router dinamic

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,28 +1,18 @@
-/*import express, { Router, RequestHandler } from "express";
+import express, { Router } from "express";
 import fs from "fs";
-import { loginCtrl, registerCtrl } from "../controllers/auth.ctrl";
-
 const router: Router = express.Router();
 const pathRouter: string = __dirname;
-
-const removeExtension = (fileName: string): string => {
-  return fileName.split(".").shift() || "";
+const removeExtencion = (fileName: string) => {
+  return fileName.split(".").shift();
 };
-
-fs.readdirSync(pathRouter).filter((file: string) => {
-  const fileWithOutExt: string = removeExtension(file);
-
-  const skip: boolean = ["index"].includes(fileWithOutExt);
-
-  if (!skip) {
-    const routePath: string = `/${fileWithOutExt}`;
-
-    const routeModule: RequestHandler =
-      require(`./${fileWithOutExt}.router`).default;
-
-    router.use(routePath, routeModule);
+fs.readdirSync(pathRouter).filter((file) => {
+  const name = removeExtencion(file);
+  if (name !== "index") {
+    console.log(name,file)
+    import(`./${file}`).then((module) => {
+      router.use(`/${name}`, module.default);
+    });
   }
 });
-router.use('/v2',(req,res)=>{res.send('hola')})
+
 export default router;
-*/
