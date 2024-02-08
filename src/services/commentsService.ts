e5c7ac79bb51a924f0e7e75ede2ce37afba4410b
Santiago <79413503+santiagoweb212@users.noreply.github.com>
2024-02-07 19:51:56 -0500

    update (#6)
    ok

---
+++ src/services/commentsService.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from ".prisma/client";
+const prisma = new PrismaClient();
+export class CommentsService {
+  static async getComments() {
+    const data = await prisma.comments.findMany();
+ 
+    return { data };
+  }
+  static async createComment(userId: number, postId: number, comment: string) {
+    const data = await prisma.comments.create({
+      data: {
+        user_id: userId,
+        post_id: postId,
+        comment,
+        timestamp: new Date(),
+      },
+    });
+    return { data };
+  }
+  static async deleteComment(commentId: number) {
+    const data = await prisma.comments.delete({
+      where: {
+        id: commentId,
+      },
+    });
+    return { data };
+  }
+
+  static async updateComment(commentId: number, comment: string) {
+    const data = await prisma.comments.update({
+      where: {
+        id: commentId,
+      },
+      data: {
+        comment,
+        timestamp: new Date(),
+      },
+    });
+    return { data };
+  }
+  
+  static async getCommentsByPostId(postId: number) {
+    const data = await prisma.comments.findMany({
+      where: {
+        post_id: postId,
+      },
+    });
+    return { data };
+  }
+}
